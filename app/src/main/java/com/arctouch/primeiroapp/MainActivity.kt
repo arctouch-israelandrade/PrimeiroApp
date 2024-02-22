1073771dbebcb75c6ab4cd72672732a564cf4e0e
Israel Andrade <israel.andrade@arctouch.com>
2024-02-22 15:31:18 -0300

    initial commit

---
+++ app/src/main/java/com/arctouch/primeiroapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.arctouch.primeiroapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.arctouch.primeiroapp.ui.theme.PrimeiroAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PrimeiroAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PerfilScreem()
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/arctouch/primeiroapp/PerfilScreem.kt
@@ -0,0 +1,10 @@
+package com.arctouch.primeiroapp
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun  PerfilScreem(){
+    Text(text = "Boa tarde!")
+}
