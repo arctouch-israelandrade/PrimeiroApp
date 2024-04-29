d5d27017b604c19590482ff4ab6e5f9e8cce3fcc
Israel Andrade <israel.andrade@arctouch.com>
2024-04-29 15:18:31 -0300

    adicionando sroll

--- app/src/main/java/com/arctouch/primeiroapp/ui/theme/DetailsScreen.kt
+++ app/src/main/java/com/arctouch/primeiroapp/ui/theme/DetailsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,13 +21,11 @@ import com.arctouch.primeiroapp.Filme
 @Composable
 fun DetailsScreen(filme: Filme?) {
 
-    
-    Column {
-
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .background(color = Color.Black)
+                .verticalScroll(rememberScrollState())
         ) {
             Column {
 
@@ -62,4 +61,4 @@ fun DetailsScreen(filme: Filme?) {
         }
 
     }
-}
+
