e0b80985299ba8073f29e76b67ff67994f946ff8
Israel Andrade <israel.andrade@arctouch.com>
2024-05-03 13:58:26 -0300

    arrumando descrição

--- app/src/main/java/com/arctouch/primeiroapp/HomeScreen.kt
+++ app/src/main/java/com/arctouch/primeiroapp/HomeScreen.kt
@@ -176,13 +176,10 @@ fun Homescreen(navController: NavHostController) {
         ),
         Filme(
             "A Marcha dos Pingüins",
-            "\"A Marcha dos Pingüins\" é um documentário emocionante que retrata a jornada extraordinária dos pinguins-imperadores na Antártida. Narrado por Morgan Freeman na versão em inglês, o filme mostra a incrível luta pela sobrevivência dessas aves durante sua migração anual para o local de reprodução, o desafio de incubação dos ovos e o cuidado com os filhotes em um ambiente extremamente hostil. Com imagens deslumbrantes e uma narrativa envolvente, o documentário cativa o público ao revelar a beleza e a determinação dessas criaturas incríveis jhgciu1ovo1hecoug1iehvl1vui hob78tfyhu1jfyfyuydveghhui \n Adicionamos verticalScroll(scrollState) ao Box que contém o conteúdo da tela de detalhes. Isso adiciona o comportamento de rolagem vertical ao conteúdo.\n",
+            "\"A Marcha dos Pingüins\" é um documentário emocionante que retrata a jornada extraordinária dos pinguins-imperadores na Antártida. Narrado por Morgan Freeman na versão em inglês, o filme mostra a incrível luta pela sobrevivência dessas aves durante sua migração anual para o local de reprodução, o desafio de incubação dos ovos e o cuidado com os filhotes em um ambiente extremamente hostil. Com imagens deslumbrantes e uma narrativa envolvente, o documentário cativa o público ao revelar a beleza e a determinação dessas criaturas incríveis",
             "Em documentários, não há um elenco de atores tradicionais, já que o filme é uma representação da vida real. No entanto, na versão em inglês de \"A Marcha dos Pingüins\", Morgan Freeman emprestou sua voz para a narração, fornecendo uma voz envolvente e informativa para o documentário. Portanto, ele é frequentemente associado ao filme como o narrador na versão em inglês. Fora isso, não há elenco de atores como em filmes de ficção, já que o filme se concentra na vida selvagem e na observação da natureza."
         ),
     )
-
-
-    Column {
 
 
         LazyColumn {
@@ -204,7 +201,6 @@ fun Homescreen(navController: NavHostController) {
 
         }
     }
-}
 
 
 
