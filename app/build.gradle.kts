360b98ff63f42b86ecada614a303a88e3808092d
Israel Andrade <israel.andrade@arctouch.com>
2024-05-09 15:09:59 -0300

    clean up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.9.0")status
+    implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
