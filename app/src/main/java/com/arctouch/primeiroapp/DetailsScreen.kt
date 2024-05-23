8ce59b8f104257f3da802982ba5c7b1fa8444944
Israel Andrade <israel.andrade@arctouch.com>
2024-05-23 17:06:55 -0300

    ajustando texto de descrição

--- app/src/main/java/com/arctouch/primeiroapp/DetailsScreen.kt
+++ app/src/main/java/com/arctouch/primeiroapp/DetailsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ fun DetailsScreen(filme: Filme?) {
             .fillMaxSize()
             .background(color = Color.Black)
             .verticalScroll(rememberScrollState())
+            .padding(16.dp)
     ) {
         Column {
 
