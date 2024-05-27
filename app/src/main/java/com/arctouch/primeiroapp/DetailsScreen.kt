9095dadb974ab9f0a7630fe1ffd7006df2caf232
Israel Andrade <israel.andrade@arctouch.com>
2024-05-27 14:45:10 -0300

    Adicionando o gitignore

--- app/src/main/java/com/arctouch/primeiroapp/DetailsScreen.kt
+++ app/src/main/java/com/arctouch/primeiroapp/DetailsScreen.kt
@@ -14,6 +14,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.arctouch.primeiroapp.Filme
