e3704ba96ae7a591d852080c805613d316c22a8f
Israel Andrade <israel.andrade@arctouch.com>
2024-08-22 16:04:49 -0300

    Adicionando biblioteca coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.moshi:moshi:1.15.1")
     implementation ("com.squareup.retrofit2:retrofit:2.11.0")
