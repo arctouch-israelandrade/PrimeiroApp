a187dd48c6a862640056a8e3ac93a2b0ef988b30
israelandrade <israel.andrade@arctouch.com>
2024-10-16 14:30:19 -0300

    Integrando a classe de estado

---
+++ app/src/main/java/com/arctouch/primeiroapp/FilmesUiState.kt
@@ -0,0 +1,4 @@
+package com.arctouch.primeiroapp
+
+class FilmesUiState {
+}
--- app/src/main/java/com/arctouch/primeiroapp/FilmesViewModel.kt
+++ app/src/main/java/com/arctouch/primeiroapp/FilmesViewModel.kt
@@ -1,15 +1,16 @@
 package com.arctouch.primeiroapp
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.arctouch.primeiroapp.repository.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
 class FilmesViewModel @Inject constructor(val filmes: FilmesRepository) : ViewModel() {
+    val uiState = MutableStateFlow<UiState.FilmesUiState>(UiState.FilmesUiState.Carregado(listOf()))
+
     fun buscarFilmes(): List<Filme> =
         runBlocking {
             return@runBlocking filmes.buscarFilmes()
