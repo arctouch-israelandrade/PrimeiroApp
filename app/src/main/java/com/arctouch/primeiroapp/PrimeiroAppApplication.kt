7682698fceedb47e0b2c81e2ebdecfd0a13c8718
israelandrade <israel.andrade@arctouch.com>
2024-10-16 14:33:35 -0300

    Removendo override da nova branch

--- app/src/main/java/com/arctouch/primeiroapp/PrimeiroAppApplication.kt
+++ app/src/main/java/com/arctouch/primeiroapp/PrimeiroAppApplication.kt
@@ -1,30 +1,10 @@
 package com.arctouch.primeiroapp
 
 import android.app.Application
-import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class PrimeiroAppApplication : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        val apiService = BuilderRetrofit().retrofit.create(ApiService::class.java)
-        val applicationScope = MainScope()
-        applicationScope.launch {
-            val result = apiService.buscarFilmes(CHAVE_API)
-            if (result.isSuccessful) {
-                val filmes = result.body()
-                Log.d("israel", filmes.toString())
-            } else {
-                Log.d("israel", result.message())
-            }
-
-        }
-
-
-    }
 
 
 }
