b763270ad5e12a45e3222843f5d799460aa30c37
israelandrade <israel.andrade@arctouch.com>
2024-10-31 15:12:31 -0300

    removendo clesse alterando para interface e excluindo UiState

--- app/src/main/java/com/arctouch/primeiroapp/FilmesUiState.kt
+++ app/src/main/java/com/arctouch/primeiroapp/FilmesUiState.kt
@@ -2,11 +2,9 @@ package com.arctouch.primeiroapp.repository
 
 import com.arctouch.primeiroapp.Filme
 
-sealed interface UiState {
 
-    sealed class FilmesUiState : UiState {
-        data class Carregado(val filmes: List<Filme>) : FilmesUiState()
-        data object Carregando : FilmesUiState()
-        data class Erro(val mensgem: String) : FilmesUiState()
+sealed interface FilmesUiState {
+        data class Carregado(val filmes: List<Filme>) : FilmesUiState
+        data object Carregando : FilmesUiState
+        data class Erro(val mensgem: String) : FilmesUiState
     }
-}
