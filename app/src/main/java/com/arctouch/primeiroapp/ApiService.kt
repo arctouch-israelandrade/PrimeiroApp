372048c12200a3630ac6029ce93a5b0c8a6922b5
israelandrade <israel.andrade@arctouch.com>
2024-12-30 15:00:11 -0300

    Conexão com a internet

--- app/src/main/java/com/arctouch/primeiroapp/ApiService.kt
+++ app/src/main/java/com/arctouch/primeiroapp/ApiService.kt
@@ -1,15 +1,9 @@
 package com.arctouch.primeiroapp
-
 import com.arctouch.primeiroapp.models.RespostaApi
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
-
-
 interface ApiService {
-
     @GET(value = "filmes")
     suspend fun buscarFilmes(@Query("key") apiKey: String): Response<RespostaApi>
-
 }
--- app/src/main/java/com/arctouch/primeiroapp/FilmesViewModel.kt
+++ app/src/main/java/com/arctouch/primeiroapp/FilmesViewModel.kt
@@ -1,9 +1,16 @@
 package com.arctouch.primeiroapp
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.arctouch.primeiroapp.repository.FilmesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -11,17 +18,36 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FilmesViewModel @Inject constructor(
-
     private val filmes: FilmesRepository,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow<FilmesUiState>(FilmesUiState.Carregando)
     val uiState = _uiState.asStateFlow()
 
+    private val _connectionState = MutableLiveData<String>()
+    val connectionState: LiveData<String> = _connectionState
+
     fun buscarFilmes() {
         viewModelScope.launch {
-            val listaDeFilme = filmes.buscarFilmes()
-            _uiState.value = FilmesUiState.Carregado(listaDeFilme)
+            if (internetVerif(context)) {
+                val listaDeFilme = filmes.buscarFilmes()
+                _uiState.value = FilmesUiState.Carregado(listaDeFilme)
+                _connectionState.value = "Conexão disponível: Filmes carregados com sucesso."
+                Log.d("Conexão", "Internet disponível")
+            } else {
+                _uiState.value = FilmesUiState.Carregando
+                _connectionState.value = "Sem conexão com a internet. Verifique sua conexão."
+                Log.e("Conexão", "Sem conexão com a internet")
+            }
         }
+    }
+
+    private fun internetVerif (context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
     }
 }
